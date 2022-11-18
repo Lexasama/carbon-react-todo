74cb6867e5d891ba899fac83cf438f55083ba074
axel-sanon <axel.sanon@carbon-it.com>
2022-11-18 10:37:22 +0100

    feat: create a general footer

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.css';
 import TodoPage from "../todos/todo-page/TodoPage";
+import TheFooter from "../general/the-footer";
 
 function App() {
     return (
         <>
             <TodoPage/>
-            <footer className="info">
-                <p>Double-click to edit a todo</p>
-                <p>Created by <a href="http://todomvc.com">Axel S.</a></p>
-                <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-            </footer>
+            <TheFooter/>
         </>
     );
 }
