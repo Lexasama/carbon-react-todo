122fe300c2f9c47a8f6d12d283da3fbdc4024123
axel-sanon <axel.sanon@carbon-it.com>
2022-11-24 15:19:19 +0100

    feat: can use local storage with type

---
+++ src/hooks/use-local-storage.hook.ts
@@ -0,0 +1,27 @@
+import {Dispatch, SetStateAction, useEffect, useState} from "react";
+
+function getLocalStorageValue<T>(key: string, defaultValue: T): T {
+    const saved = localStorage.getItem(key);
+
+    if (saved) {
+        return (JSON.parse(saved) as T);
+    }
+    return defaultValue;
+
+}
+
+type SetValue<T> = Dispatch<SetStateAction<T>>
+
+function useLocalStorage<T>(key: string, defaultValue: T): [T, SetValue<T>] {
+    const [value, setValue] = useState<T>(() => {
+        return getLocalStorageValue<T>(key, defaultValue);
+    });
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value]);
+
+    return [value, setValue];
+}
+
+export default useLocalStorage;
