53f69e319a2577c979ca48cd26227880a692b1dd
axel-sanon <axel.sanon@carbon-it.com>
2022-11-25 16:56:51 +0100

    feat: delete useless component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import TodoPage from "../todos/todo-page/todo-page";
-import TheFooter from "../general/the-footer";
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import {TodoListFilter} from "../todos/TodoListFilter";
 
@@ -14,7 +13,11 @@ function App() {
                 <Route path="/completed" element={<TodoPage filter={TodoListFilter.COMPLETED}/>}/>
                 <Route path="/active" element={<TodoPage filter={TodoListFilter.ACTIVE}/>}/>
             </Routes>
-            <TheFooter/>
+            <footer className="info">
+                <p>Double-click to edit a todo</p>
+                <p>Created by <a href="http://todomvc.com">Axel S.</a></p>
+                <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+            </footer>
         </BrowserRouter>
     );
 }
